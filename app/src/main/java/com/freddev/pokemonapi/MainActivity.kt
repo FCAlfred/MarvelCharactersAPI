9c7345bd6d832b39d5ff028025e9257b053592a1
Alfredo Molina <fca1522@gmail.com>
2024-01-11 17:17:34 -0600

    ViewModel - LiveData - viewModel - LiveData implementation on project

--- app/src/main/java/com/freddev/pokemonapi/MainActivity.kt
+++ app/src/main/java/com/freddev/pokemonapi/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.freddev.pokemonapi
 
-import com.freddev.pokemonapi.model.network.MarvelRepository
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.freddev.pokemonapi.viewmodel.MarvelViewModel
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var viewModel: MarvelViewModel
+
+    override
+    fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModelProvider(this)[MarvelViewModel::class.java]
         val textView = findViewById<TextView>(R.id.text_view)
 
-        MarvelRepository.getMarvelCharacters(
-            onSuccess = { character ->
-                textView.text = character[0].name
-            },
-            onError = {
-                textView.text = it.toString()
-            }
-        )
-
+        viewModel.charactersList.observe(this, Observer {
+            textView.text = it[0].name
+        })
 
     }
 }
--- app/src/main/java/com/freddev/pokemonapi/viewmodel/MarvelViewModel.kt
+++ app/src/main/java/com/freddev/pokemonapi/viewmodel/MarvelViewModel.kt
@@ -1,9 +1,32 @@
 package com.freddev.pokemonapi.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.freddev.pokemonapi.model.network.MarvelRepository
 import com.freddev.pokemonapi.model.network.data.MarvelCharacter
 
 class MarvelViewModel : ViewModel() {
+
+    private var _charactersList = MutableLiveData<List<MarvelCharacter>>()
+    val charactersList: LiveData<List<MarvelCharacter>> get() = _charactersList
+
+
+    init {
+        getCharacters()
+    }
+
+    private fun getCharacters() {
+        MarvelRepository.getMarvelCharacters(
+            onSuccess = { characters ->
+                _charactersList.postValue(characters)
+            },
+            onError = {
+                //LLamar a ROOM DB
+                Log.i("NETWORK ERROR", it.toString())
+            }
+        )
+    }
 
 }
