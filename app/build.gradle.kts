4d94c0c96629ea8ee88b18f8e1926b5a5777a6c8
Alfredo Molina <fca1522@gmail.com>
2024-01-12 11:03:19 -0600

    Navigation - Navigation Implementation with fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -63,5 +63,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
-
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.6")
 }
--- app/src/main/java/com/freddev/pokemonapi/MainActivity.kt
+++ app/src/main/java/com/freddev/pokemonapi/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.freddev.pokemonapi.databinding.ActivityMainBinding
 import com.freddev.pokemonapi.model.network.data.MarvelCharacter
@@ -11,29 +13,29 @@ import com.freddev.pokemonapi.view.MovieActions
 import com.freddev.pokemonapi.view.adapters.CharactersAdapter
 import com.freddev.pokemonapi.viewmodel.MarvelViewModel
 
-class MainActivity : AppCompatActivity(), MovieActions {
+class MainActivity : AppCompatActivity()/*, MovieActions*/ {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var charactersAdapter: CharactersAdapter
+    private lateinit var navController: NavController
+    //    private lateinit var charactersAdapter: CharactersAdapter
     private lateinit var viewModel: MarvelViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this)[MarvelViewModel::class.java]
-        charactersAdapter = CharactersAdapter(emptyList(), this)
-        binding.recyclerHeroes.layoutManager = GridLayoutManager(this, 3)
-        binding.recyclerHeroes.adapter = charactersAdapter
+        setContentView(binding.root)
+        /*        charactersAdapter = CharactersAdapter(emptyList(), this)
+                binding.recyclerHeroes.layoutManager = GridLayoutManager(this, 3)
+                binding.recyclerHeroes.adapter = charactersAdapter
 
-        viewModel.charactersList.observe(this) { marvelList ->
-            charactersAdapter.updateList(marvelList)
-            Toast.makeText(this, "elements:${marvelList.size}", Toast.LENGTH_SHORT).show()
-        }
+                viewModel.charactersList.observe(this) { marvelList ->
+                    charactersAdapter.updateList(marvelList)
+                    Toast.makeText(this, "elements:${marvelList.size}", Toast.LENGTH_SHORT).show()
+                }*/
     }
 
-    override fun onClickedChar(selectedChar: MarvelCharacter) {
+/*    override fun onClickedChar(selectedChar: MarvelCharacter) {
         Toast.makeText(this, "Clicked ${selectedChar.name}", Toast.LENGTH_SHORT).show()
-    }
+    }*/
 }
